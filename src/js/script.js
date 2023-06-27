a8bcaf2dccd059120a36e57df84ea456794deb31
Aleksei Briukhachev <abriukhachev@trask.cz>
2023-06-27 18:23:14 +0200

    Adaptation

--- src/js/script.js
+++ src/js/script.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg" alt="left"></button>',
         responsive: [
             {
-              breakpoint: 768,
+              breakpoint: 20,
               settings: {
                 dots: true,
                 arrows: false
